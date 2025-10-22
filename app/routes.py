c451257ad83ecffc27022b896769eb1e11c8d408
randywanyoike <randywanyoike@gmail.com>
2025-10-22 20:41:13 +0300

    G14-4 backend- Core system and database

---
+++ app/routes.py
@@ -0,0 +1,96 @@
+from flask import Blueprint, request, jsonify
+from app import db
+from app.models import User, WasteBin, CollectionSchedule, RecyclingRecord
+from app.schemas import UserSchema, WasteBinSchema, ScheduleSchema, RecyclingSchema
+
+main = Blueprint('main', __name__)
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+bin_schema = WasteBinSchema()
+bins_schema = WasteBinSchema(many=True)
+schedule_schema = ScheduleSchema()
+schedules_schema = ScheduleSchema(many=True)
+recycle_schema = RecyclingSchema()
+recycles_schema = RecyclingSchema(many=True)
+
+@main.route('/api/users', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    new_user = User(name=data['name'], email=data['email'], role=data.get('role', 'resident'))
+    db.session.add(new_user)
+    db.session.commit()
+    return user_schema.jsonify(new_user), 201
+
+@main.route('/api/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return users_schema.jsonify(users)
+
+@main.route('/api/users/<int:id>', methods=['PUT'])
+def update_user(id):
+    user = User.query.get_or_404(id)
+    data = request.get_json()
+    user.name = data.get('name', user.name)
+    user.email = data.get('email', user.email)
+    user.role = data.get('role', user.role)
+    db.session.commit()
+    return user_schema.jsonify(user)
+
+@main.route('/api/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get_or_404(id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": "User deleted successfully"}), 200
+
+
+@main.route('/api/bins', methods=['POST'])
+def add_bin():
+    data = request.get_json()
+    new_bin = WasteBin(location=data['location'], capacity=data['capacity'])
+    db.session.add(new_bin)
+    db.session.commit()
+    return bin_schema.jsonify(new_bin), 201
+
+@main.route('/api/bins', methods=['GET'])
+def get_bins():
+    bins = WasteBin.query.all()
+    return bins_schema.jsonify(bins)
+
+
+@main.route('/api/schedules', methods=['POST'])
+def add_schedule():
+    data = request.get_json()
+    new_schedule = CollectionSchedule(
+        bin_id=data['bin_id'],
+        driver_id=data['driver_id'],
+        collection_date=data['collection_date']
+    )
+    db.session.add(new_schedule)
+    db.session.commit()
+    return schedule_schema.jsonify(new_schedule), 201
+
+@main.route('/api/schedules', methods=['GET'])
+def get_schedules():
+    schedules = CollectionSchedule.query.all()
+    return schedules_schema.jsonify(schedules)
+
+
+@main.route('/api/recycling', methods=['POST'])
+def add_recycling():
+    data = request.get_json()
+    new_record = RecyclingRecord(
+        type=data['type'],
+        weight=data['weight'],
+        date=data['date'],
+        collected_by=data['collected_by']
+    )
+    db.session.add(new_record)
+    db.session.commit()
+    return recycle_schema.jsonify(new_record), 201
+
+@main.route('/api/recycling', methods=['GET'])
+def get_recycling():
+    records = RecyclingRecord.query.all()
+    return recycles_schema.jsonify(records)
