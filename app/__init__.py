4ffc6696c141d26508320c55d97f85271a52827b
randywanyoike <randywanyoike@gmail.com>
2025-10-24 19:41:23 +0300

    G14-4 backend- Core system and database

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from app.config import Config
 
- 
 db = SQLAlchemy()
-ma = Marshmallow()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)  
-
-    
-    app.config.from_object('app.config.Config')
+    app.config.from_object(Config)
 
     db.init_app(app)
-    ma.init_app(app)
+    jwt.init_app(app)
 
-    
     from app.routes import main
     app.register_blueprint(main)
 
