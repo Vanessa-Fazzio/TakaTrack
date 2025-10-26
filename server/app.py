9b06539d71ddae5f889c7d97b52a939a5b21b469
feysalabdi51 <pheysalupdyy@gmail.com>
2025-10-27 02:10:02 +0300

    G14-3 Add Flask-SocketIO and Flask-Mail integration with email sending functionality
    - Updated Pipfile to include flask-socketio, flask-mail, sqlalchemy-serializer, load-dotenv, and dotenv.
    - Modified Pipfile.lock to reflect the new dependencies and their versions.
    - Enhanced app.py to configure Flask-Mail with environment variables and added a route to send test emails.
    - Created a .env file to store sensitive email configuration details.

--- server/app.py
+++ server/app.py
@@ -4,28 +4,51 @@ from flask_bcrypt import Bcrypt
 from flask_restful import Api, Resource
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from datetime import  timedelta
-from flask_jwt_extended import  JWTManager
+from datetime import timedelta
+from flask_jwt_extended import JWTManager
 from flask_socketio import SocketIO, emit
 from model import CollectionPoint, Collector, db
-
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///TakaTrack.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
-jwt = JWTManager(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///TakaTrack.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
 
 db.init_app(app)
-migrate=Migrate(app,db)
-
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
 CORS(app)
-bcrypt=Bcrypt(app)
 api = Api(app)
-
-CORS(app)
+jwt = JWTManager(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
+mail = Mail(app)
+
+
+@app.route('/send')
+def send():
+    message = Message(
+        subject="Hello from TakaTrack",
+        recipients=["test.mailtrap1234@gmail.com"],
+        sender=('Peter from TakaTrack', 'peter@mailtrap.club')
+    )
+    message.body = "This is a test email sent from the TakaTrack Flask application using Mailtrap."
+    mail.send(message)
+    return "Email sent successfully!"
+
 
 class CollectionPointsResource(Resource):
     def get(self):
@@ -48,7 +71,7 @@ class CollectionPointsResource(Resource):
         return new_point.serialize(), 201
 
 
-# Collector current location
+
 class CollectorResource(Resource):
     def get(self):
         collector = Collector.query.first()
